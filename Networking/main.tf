ad88826a5441881f5f83d75127b96c72d84fb82a
WMoorby <WMoorby@spartaglobal.com>
2021-09-16 21:50:04 +0100

    main.tf: Fixed SSH connection and added DB subnet/securitygroup/instance. output.rf: Added SSH command print

--- Networking/main.tf
+++ Networking/main.tf
@@ -25,19 +25,19 @@ resource "aws_internet_gateway" "sre_will_IG" {
     }
 }
 
-# Create a subnet
+# Create a public subnet (for app instance)
 
-resource "aws_subnet" "sre_will_subnet_tf" {
+resource "aws_subnet" "sre_will_public_subnet_tf" {
     vpc_id = aws_vpc.sre_will_vpc_tf.id
-    cidr_block = var.subnet_CIDR_block
+    cidr_block = var.public_subnet_CIDR_block
     map_public_ip_on_launch = true
 
     tags = {
-        Name = "sre_will_subnet_tf"
+        Name = "sre_will_public_subnet_tf"
     }
 }
 
-# Edit security group rules
+# Create security group for app instance
 
 resource "aws_security_group" "sre_will_app_group" {
     name = "sre_will_app_sg_tf"
@@ -57,7 +57,7 @@ resource "aws_security_group" "sre_will_app_group" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = [var.public_CIDR_block]
+        cidr_blocks = [var.private_ip]
     }
 
     # Port 3000 for reverse proxy
@@ -94,13 +94,13 @@ resource "aws_route" "sre_will_route_table" {
 ## Need to define all the information required to launch the instance
 
 resource "aws_instance" "app_instance" {
-    ami = var.ami_id
+    ami = var.app_ami_id
     instance_type ="t2.micro"
     associate_public_ip_address = true
     vpc_security_group_ids = [
         aws_security_group.sre_will_app_group.id
     ]
-    subnet_id = aws_subnet.sre_will_subnet_tf.id
+    subnet_id = aws_subnet.sre_will_public_subnet_tf.id
 
     tags = {
         Name = "sre_will_terraform_app"
@@ -111,15 +111,105 @@ resource "aws_instance" "app_instance" {
     connection {
         type = "ssh"
         user = "ubuntu"
-        private_key = var.aws_key_path
-        host = "${self.associate_public_ip_address}"
+        private_key = file(var.aws_key_path)
+        host = aws_instance.app_instance.public_ip
     }
 
-    # provisioner "remote-exec" {
-    #     inline = [
-    #         "cd app",
-    #         "pm2 kill",
-    #         "pm2 start app.js"
-    #     ]
-    # }
+    provisioner "remote-exec" {
+        inline = [
+            "sudo mkdir testdir"
+        ]
+    }
 }
+
+# Create private subnet (for db instance)
+
+resource "aws_subnet" "sre_will_private_subnet_tf" {
+    vpc_id = aws_vpc.sre_will_vpc_tf.id
+    cidr_block = var.private_subnet_CIDR_block
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "sre_will_private_subnet_tf"
+    }
+}
+
+# Create security group for db instance
+
+resource "aws_security_group" "sre_will_db_group" {
+    name = "sre_will_db_sg_tf"
+    description = "sre_will_db_sg_tf"
+    vpc_id = aws_vpc.sre_will_vpc_tf.id
+
+    # SSH port
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.private_ip]
+    }
+
+    # Port 27017 for DB
+    ingress {
+        from_port = 27017
+        to_port = 27017
+        protocol = "tcp"
+        cidr_blocks = ["${aws_instance.app_instance.public_ip}/32"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [var.public_CIDR_block]
+    }
+
+    tags = {
+        Name = "sre_will_db_sg_tf"
+    }
+}
+
+resource "aws_instance" "db_instance" {
+    ami = var.db_ami_id
+    instance_type = "t2.micro"
+    associate_public_ip_address = false
+    vpc_security_group_ids = [
+        aws_security_group.sre_will_db_group.id
+    ]
+    subnet_id = aws_subnet.sre_will_private_subnet_tf.id
+
+    tags = {
+        Name = "sre_will_terraform_db"
+    }
+
+    key_name = var.aws_key_name
+
+    connection {
+        type = "ssh"
+        user = "ubuntu"
+        private_key = var.aws_key_path
+        host = aws_instance.db_instance.public_ip
+    }
+}
+
+
+## Need to finish configuring the db instance
+## How to SSH into instance subnet is private and instance has no public IP??
+## Does "connection" section of instance definition still work??
+
+
+## Was getting infinite loop on SSH connection for app instance
+## Error was >>
+
+## interrupted - last error: dial tcp: lookup true: no such host
+
+## Changed host variable in connection{}
+## >> host = aws_instance.app_instance.public_ip
+
+## New error for provisioner "remote-exec" {} >>
+
+## aws_instance.app_instance (remote-exec): sudo: unable to resolve host ip-10-105-1-151
+
+## Either enableDnsHostname in VPC configuration
+## https://stackoverflow.com/questions/33441873/aws-error-sudo-unable-to-resolve-host-ip-10-0-xx-xx
+## Or add dns hostname to /etc/hosts
+## https://stackoverflow.com/questions/37517630/aws-linux-sudo-unable-to-resolve-host
--- Networking/output.tf
+++ Networking/output.tf
@@ -1,21 +1,25 @@
 # output.tf > outputs whatever you define to the terminal
 
-output "vpc_id" {
-    value = "${aws_vpc.sre_will_vpc_tf.id}"
-}
+# output "vpc_id" {
+#     value = "${aws_vpc.sre_will_vpc_tf.id}"
+# }
 
-output "internet_gateway_id" {
-    value = "${aws_internet_gateway.sre_will_IG.id}"
-}
+# output "internet_gateway_id" {
+#     value = "${aws_internet_gateway.sre_will_IG.id}"
+# }
 
-output "subnet_id" {
-    value = "${aws_subnet.sre_will_subnet_tf.id}"
-}
+# output "subnet_id" {
+#     value = "${aws_subnet.sre_will_public_subnet_tf.id}"
+# }
 
-output "security_group_id" {
-    value = "${aws_security_group.sre_will_app_group.id}"
-}
+# output "security_group_id" {
+#     value = "${aws_security_group.sre_will_app_group.id}"
+# }
 
-output "route_table_id" {
-    value = "${aws_vpc.sre_will_vpc_tf.default_route_table_id}"
+# output "route_table_id" {
+#     value = "${aws_vpc.sre_will_vpc_tf.default_route_table_id}"
+# }
+
+output "app_instance_SSH_command" {
+    value = "ssh -i ${var.aws_key_path} ubuntu@${aws_instance.app_instance.public_ip}"
 }
