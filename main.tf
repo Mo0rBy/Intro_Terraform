3420e99f30cbae862011f2f031dfcd53835d8135
WMoorby <WMoorby@spartaglobal.com>
2021-09-16 15:55:54 +0100

    completed main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,133 @@
-# Let's set up our cloud provider with Terraform
+# # Let's set up our cloud provider with Terraform
 
 provider "aws" {
     region = "eu-west-1"
 }
 
+######################
+# Step 1 - Create a VPC with CIDR block
+# Step 2 - Run terraform plan then terraform apply
+# Step 3 - Get teh VPC ID from AWS or terraform logs, add the ID to the variable.terraform
+
+
+######################
+
+# Create a VPC
+
+resource "aws_vpc" "sre_will_vpc_tf" {
+    cidr_block = var.vpc_CIDR_block
+    instance_tenancy = "default"
+
+    tags = {
+        Name = "sre_will_vpc_tf"
+    }
+}
+
+# vpc_id created >> [id=vpc-0ead78dc96892a665]
+
+# Create an Internet Gateway
+
+resource "aws_internet_gateway" "sre_will_IG" {
+    vpc_id = var.vpc_id
+
+    tags = {
+        Name = "sre_will_IG_tf"
+    }
+}
+
+# Create a subnet
+
+resource "aws_subnet" "sre_will_subnet_tf" {
+    vpc_id = var.vpc_id
+    cidr_block = var.subnet_CIDR_block
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "sre_will_subnet_tf"
+    }
+}
+
+# Edit security group rules
+
+resource "aws_security_group" "sre_will_app_group" {
+    name = "sre_will_app_sg_tf"
+    description = "sre_will_app_sg_tf"
+    vpc_id = var.vpc_id
+
+    # HTTP port, global access
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [var.public_CIDR_block]
+    }
+
+    # SSH port, (set to 0.0.0.0/0 for global access)
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.public_CIDR_block]
+    }
+
+    # Port 3000 for reverse proxy
+    ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = [var.public_CIDR_block]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [var.public_CIDR_block]
+    }
+
+    tags = {
+        Name = "sre_will_app_sg_tf"
+    }
+}
+
+# Edit the route table (thats created with the VPC)
+## Adding the route to the internet gateway
+
+resource "aws_route" "r" {
+    route_table_id = var.route_table_id
+    destination_cidr_block = var.public_CIDR_block
+    gateway_id = var.internet_gateway_id
+}
+
 ## Let's launch an EC2 instance using the app AMI
 # Need to define all the information required to launch the instance
 resource "aws_instance" "app_instance" {
-    ami = "ami-04f364e11ef840257"
+    ami = var.ami_id
     instance_type ="t2.micro"
     associate_public_ip_address = true
+    vpc_security_group_ids = [
+        var.app_security_group_id
+    ]
+    subnet_id = var.subnet_id
+
     tags = {
         Name = "sre_will_terraform_app"
+    }
+
+    key_name = var.aws_key_name
+
+    connection {
+        type = "ssh"
+        user = "ubuntu"
+        private_key = var.aws_key_path
+        host = "${self.associate_public_ip_address}"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "cd app",
+            "pm2 kill",
+            "pm2 start app.js"
+        ]
     }
 }
